eb9524a92d76519ddda5f7d4b1fea12ff467b73e
jyekesho <jyekesho@gmail.com>
2023-02-13 21:00:02 -0600

    updated

---
+++ src/Task.js
@@ -0,0 +1,10 @@
+export const Task = (props) => {
+    return (
+        <div className="task" style ={{backgroundColor: props.completed ? "black" : "white"}}>
+            <h1>{props.taskName}</h1>
+            <button onClick={() => props.completeTask(props.id)}>COmplete</button>
+            <button onClick={() => props.deleteTask(props.id)}>X</button>
+
+        </div>
+    );
+};
